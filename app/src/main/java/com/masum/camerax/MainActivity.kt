987e6a6ae0a6ba0300ddf8664fb4ab675e22c245
mdmasum.shuvo <mdmasum.shuvo.cse@gmail.com>
2019-10-26 17:18:10 +0600

    cameraX

--- app/src/main/java/com/masum/camerax/MainActivity.kt
+++ app/src/main/java/com/masum/camerax/MainActivity.kt
@@ -2,14 +2,24 @@ package com.masum.camerax
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.view.Surface
 import android.view.TextureView
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.nio.ByteBuffer
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 
 private const val REQUEST_CODE_PERMISION = 10
@@ -19,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
     private val executor = Executors.newSingleThreadExecutor();
     private lateinit var viewFinder: TextureView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,15 +44,97 @@ class MainActivity : AppCompatActivity() {
         viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
             updateTransform()
         }
+
+
     }
 
 
     private fun startCamera() {
 
+        val previewConfig =
+            PreviewConfig.Builder().apply { setTargetResolution(Size(640, 480)) }.build()
+
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+            viewFinder.surfaceTexture = it.surfaceTexture
+
+            updateTransform()
+        }
+
+
+        val imageCaptureConfig = ImageCaptureConfig.Builder().apply {
+            setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+        }.build()
+        val imageCapture = ImageCapture(imageCaptureConfig)
+
+        btn_capture.setOnClickListener {
+
+            val file = File(externalMediaDirs.first(), "${System.currentTimeMillis()}.jpg")
+
+            imageCapture.takePicture(file, executor,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onImageSaved(file: File) {
+                        val msg = "image captured success and save into sd card"
+
+                        viewFinder.post {
+                            Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                    override fun onError(
+                        imageCaptureError: ImageCapture.ImageCaptureError,
+                        message: String,
+                        cause: Throwable?
+                    ) {
+                        val msg = "photo captured failed"
+                        viewFinder.post {
+                            Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                })
+
+        }
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+            // In our analysis, we care more about the latest image than
+            // analyzing *every* image
+            setImageReaderMode(
+                ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE
+            )
+        }.build()
+
+        // Build the image analysis use case and instantiate our analyzer
+        val analyzerUseCase = ImageAnalysis(analyzerConfig).apply {
+            setAnalyzer(executor, LuminosityAnalyzer())
+        }
+        CameraX.bindToLifecycle(this, preview, imageCapture,analyzerUseCase)
+
     }
 
     private fun updateTransform() {
 
+        val matrix = Matrix()
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+
+        val rotationDegree = when (viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+
+            else -> {
+                return
+            }
+        }
+
+        matrix.postRotate(rotationDegree.toFloat(), centerX, centerY)
+        viewFinder.setTransform(matrix)
     }
 
 
@@ -65,5 +158,42 @@ class MainActivity : AppCompatActivity() {
 
     private fun allPermissionGranted() = REQUIRED_PERMISION.all {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
+        private var lastAnalyzedTimestamp = 0L
+
+        /**
+         * Helper extension function used to extract a byte array from an
+         * image plane buffer
+         */
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy, rotationDegrees: Int) {
+            val currentTimestamp = System.currentTimeMillis()
+            // Calculate the average luma no more often than every second
+            if (currentTimestamp - lastAnalyzedTimestamp >=
+                TimeUnit.SECONDS.toMillis(1)
+            ) {
+                // Since format in ImageAnalysis is YUV, image.planes[0]
+                // contains the Y (luminance) plane
+                val buffer = image.planes[0].buffer
+                // Extract image data from callback object
+                val data = buffer.toByteArray()
+                // Convert the data into an array of pixel values
+                val pixels = data.map { it.toInt() and 0xFF }
+                // Compute average luminance for the image
+                val luma = pixels.average()
+                // Log the new luma value
+                Log.d("CameraXApp", "Average luminosity: $luma")
+                // Update timestamp of last analyzed frame
+                lastAnalyzedTimestamp = currentTimestamp
+            }
+        }
     }
 }
